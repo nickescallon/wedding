37e5d806aa82456e8234a2525185565c64505ae2
Nick Escallon <nickescallon@gmail.com>
2016-04-16 12:54:40 -0400

    itinerary complete

--- dist/js/templates.js
+++ dist/js/templates.js
@@ -23,69 +23,156 @@ module.run(['$templateCache', function($templateCache) {
     '  <div class="wide"></div>\n' +
     '\n' +
     '  <div class="container-fluid">\n' +
-    '    <div class="col-xs-12 text-center">\n' +
+    '    <div class="text-center">\n' +
     '\n' +
-    '      <div class="row">\n' +
-    '        <div class="col-xs-3 col-sm-4 col-lg-5 line line-primary"></div>\n' +
-    '        <h2 class="col-xs-6 col-sm-4 col-lg-2 text-center primary">Welcome</h2>\n' +
-    '        <div class="col-xs-3 col-sm-4 col-lg-5 line line-primary"></div>\n' +
-    '      </div>\n' +
+    '      <div class="row-fluid section primary">\n' +
+    '        <div class="content">\n' +
+    '          <div class="row">\n' +
+    '            <div class="col-md-8 col-md-offset-2">\n' +
+    '              <h3 class="text-center inverse">WELCOME</h3>\n' +
+    '              <div class="separator"></div>\n' +
+    '            </div>\n' +
+    '          </div>\n' +
     '\n' +
-    '      <div class="row">\n' +
-    '        <div class="col-xs-12">\n' +
-    '          <p>\n' +
-    '            We\'re so excited to have you join us for our wedding in beautiful Stellenbosch, South Africa.\n' +
-    '          </p>\n' +
+    '          <div class="row">\n' +
+    '            <div class="col-xs-12">\n' +
+    '              <p>\n' +
+    '                We\'re so excited to have you join us for our wedding in beautiful Stellenbosch, South Africa.\n' +
+    '              </p>\n' +
     '\n' +
-    '          <p>\n' +
-    '            Outside of the wedding itself, we\'ve planned a few events for us to all enjoy in the days leading up to, and following the wedding day.\n' +
-    '          </p>\n' +
-    '          <p>\n' +
-    '            We\'re planning on spending some time in Cape Town, hosting a classic south African Braai for the rehearsal dinner, and traveling\n' +
-    '            closer to Johannesburg in order to spend a few nights on Safari. While we ask that you join us on October 9th and 10th for the rehearsal\n' +
-    '            and the wedding, please feel free to opt in or out of any of the other planned activities.\n' +
-    '          </p>\n' +
+    '              <p>\n' +
+    '                Outside of the wedding itself, we\'ve planned a few events for us to all enjoy in the days leading up to, and following the wedding day.\n' +
+    '              </p>\n' +
     '\n' +
-    '          <p>\n' +
-    '            We\'ve outlined the key dates below, and have more details the "Activities" page.\n' +
-    '          </p>\n' +
+    '              <p>\n' +
+    '                We\'re planning on spending some time in Cape Town, hosting a classic south African Braai for the rehearsal dinner, and traveling\n' +
+    '                closer to Johannesburg in order to spend a few nights on Safari. While we ask that you join us on October 9th and 10th for the rehearsal\n' +
+    '                and the wedding, please feel free to opt in or out of any of the other planned activities.\n' +
+    '              </p>\n' +
+    '\n' +
+    '              <p>\n' +
+    '                We\'ve outlined the key dates below, and have more details the "Activities" page.\n' +
+    '              </p>\n' +
+    '            </div>\n' +
+    '          </div>\n' +
     '        </div>\n' +
     '      </div>\n' +
     '\n' +
-    '      <!-- NEED TO DEFINE INVERSE CLASSES... #333333 BG and Gold text. Also add padding for every new "section" -->\n' +
-    '      <div class="row inverse">\n' +
-    '        <p>\n' +
-    '          We\'re still working on finalizing our plans, but our <strong>tentative itinerary is currently:</strong>\n' +
-    '        </p>\n' +
+    '      <div class="row-fluid section inverse">\n' +
     '\n' +
     '        <div class="row">\n' +
-    '          <div class="col-xs-offset-3 col-xs-2 text-right"><strong>Oct 4:</strong></div>\n' +
-    '          <div class="col-xs-6 text-left">Depart Boston for Cape Town.</div>\n' +
+    '          <div class="content">\n' +
+    '            <div class="col-md-8 col-md-offset-2">\n' +
+    '              <h3 class="text-center inverse">KEY DATES</h3>\n' +
+    '              <div class="separator"></div>\n' +
+    '            </div>\n' +
+    '          </div>\n' +
     '        </div>\n' +
     '\n' +
     '        <div class="row">\n' +
-    '          <div class="col-xs-offset-3 col-xs-2 text-right"><strong>Oct 5-8:</strong></div>\n' +
-    '          <div class="col-xs-6 text-left">Activities/Tourism in Cape Town.</div>\n' +
-    '        </div>\n' +
     '\n' +
-    '        <div class="row">\n' +
-    '          <div class="col-xs-offset-3 col-xs-2 text-right"><strong>Oct 9 - 10:</strong></div>\n' +
-    '          <div class="col-xs-6 text-left">Drive to Stellenbosch for the wedding events and wine tastings.</div>\n' +
-    '        </div>\n' +
+    '          <div class="col-lg-3 col-md-6 text-center">\n' +
+    '            <div class="box">\n' +
+    '              <h4>OCT 9</h4>\n' +
+    '              <p>\n' +
+    '                  Check into your room at the <a href="//www.zorgvlietwines.com/" target="_blank">Zorgvliet Wine Estate</a>,\n' +
+    '                  which is also where we\'ll be hosting the <strong>rehearsal dinner</strong> at 5:00pm.\n' +
+    '              <p>\n' +
+    '                The rehearsal dinner will be a traditional South Afrrican Braai.\n' +
+    '              <p>\n' +
+    '                <strong>Suggested Attire:</strong> Casual\n' +
+    '            </div>\n' +
+    '          </div>\n' +
     '\n' +
-    '        <div class="row">\n' +
-    '          <div class="col-xs-offset-3 col-xs-2 text-right"><strong>Oct 11:</strong></div>\n' +
-    '          <div class="col-xs-6 text-left">Fly to Johannesburg.</div>\n' +
-    '        </div>\n' +
     '\n' +
-    '        <div class="row">\n' +
-    '          <div class="col-xs-offset-3 col-xs-2 text-right"><strong>Oct 12 - 13:</strong></div>\n' +
-    '          <div class="col-xs-6 text-left">Drive to Tau Game Lodge for Safari.</div>\n' +
-    '        </div>\n' +
+    '          <div class="col-lg-3 col-md-6 text-center">\n' +
+    '            <div class="box">\n' +
+    '              <h4>OCT 10</h4>\n' +
+    '              <p>\n' +
+    '                <strong>Wedding day!</strong> Please join us at <a href="//www.molenvliet.co.za/" target="_blank">MolenVliet</a> at 2:30pm. We\'ll be providing transportation to and from <a href="//www.zorgvlietwines.com/" target="_blank">Zorgvliet</a>, where you\'ll be staying.\n' +
+    '              <p>\n' +
+    '                <strong>Ceremony:</strong> 3:00pm\n' +
+    '              <p>\n' +
+    '                <strong>Reception:</strong> 5:30pm\n' +
+    '              <p>\n' +
+    '                <strong>Suggested Attire:</strong> Formal\n' +
+    '            </div>\n' +
     '\n' +
-    '        <div class="row">\n' +
-    '          <div class="col-xs-offset-3 col-xs-2 text-right"><strong>Oct 14:</strong></div>\n' +
-    '          <div class="col-xs-6 text-left">Drive to Johannesburg and fly home.</div>\n' +
+    '          </div>\n' +
+    '\n' +
+    '          <div class="col-lg-3 col-md-6 text-center">\n' +
+    '            <div class="box">\n' +
+    '              <h4>OCT 11</h4>\n' +
+    '              <p>\n' +
+    '                If you\'re joining us for safari at the <a href="//www.taugamelodge.co.za/" target="_blank">Tau Game Lodge</a>,\n' +
+    '                then today we\'ll be flying to Johannseburg, and immediately departing for the <a href="//www.sun-city-south-africa.com/">Sun City Resort.</a>\n' +
+    '              <p>\n' +
+    '                We\'ll be spending one night there.\n' +
+    '              <p>\n' +
+    '                Please check our "Travel" section for suggested flights from Capetown to Johannesburg.\n' +
+    '            </div>\n' +
+    '          </div>\n' +
+    '\n' +
+    '          <div class="col-lg-3 col-md-6 text-center">\n' +
+    '            <div class="box">\n' +
+    '              <h4>OCT 12</h4>\n' +
+    '              <p>\n' +
+    '                We\'ll be departing Johannesrburg for Safari at the <a href="//www.taugamelodge.co.za/" target="_blank">Tau Game Lodge</a>.\n' +
+    '              <p>\n' +
+    '                We\'ll be spending two nights at <a href="//www.taugamelodge.co.za/" target="_blank">Tau</a>.\n' +
+    '            </div>\n' +
+    '          </div>\n' +
+    '\n' +
+    '        </div>\n' +
+    '      </div>\n' +
+    '\n' +
+    '\n' +
+    '      <div class="row-fluid section primary">\n' +
+    '      <div class="content">\n' +
+    '          <div class="row">\n' +
+    '            <div class="col-md-8 col-md-offset-2">\n' +
+    '              <h3 class="text-center primary">OUR ITINERARY</h3>\n' +
+    '              <div class="separator"></div>\n' +
+    '            </div>\n' +
+    '          </div>\n' +
+    '\n' +
+    '          <div class="row">\n' +
+    '            <div class="col-xs-12">\n' +
+    '              <p>\n' +
+    '                We\'re still working on finalizing our plans, but our <strong>tentative itinerary is currently:</strong>\n' +
+    '              </p>\n' +
+    '            </div>\n' +
+    '          </div>\n' +
+    '\n' +
+    '          <div class="row">\n' +
+    '            <div class="col-xs-offset-3 col-xs-2 text-right"><strong>Oct 4:</strong></div>\n' +
+    '            <div class="col-xs-6 text-left">Depart Boston for Cape Town.</div>\n' +
+    '          </div>\n' +
+    '\n' +
+    '          <div class="row">\n' +
+    '            <div class="col-xs-offset-3 col-xs-2 text-right"><strong>Oct 5-8:</strong></div>\n' +
+    '            <div class="col-xs-6 text-left">Activities/Tourism in Cape Town.</div>\n' +
+    '          </div>\n' +
+    '\n' +
+    '          <div class="row">\n' +
+    '            <div class="col-xs-offset-3 col-xs-2 text-right"><strong>Oct 9 - 10:</strong></div>\n' +
+    '            <div class="col-xs-6 text-left">Drive to Stellenbosch for the wedding events and wine tastings.</div>\n' +
+    '          </div>\n' +
+    '\n' +
+    '          <div class="row">\n' +
+    '            <div class="col-xs-offset-3 col-xs-2 text-right"><strong>Oct 11:</strong></div>\n' +
+    '            <div class="col-xs-6 text-left">Fly to Johannesburg.</div>\n' +
+    '          </div>\n' +
+    '\n' +
+    '          <div class="row">\n' +
+    '            <div class="col-xs-offset-3 col-xs-2 text-right"><strong>Oct 12 - 13:</strong></div>\n' +
+    '            <div class="col-xs-6 text-left">Drive to Tau Game Lodge for Safari.</div>\n' +
+    '          </div>\n' +
+    '\n' +
+    '          <div class="row">\n' +
+    '            <div class="col-xs-offset-3 col-xs-2 text-right"><strong>Oct 14:</strong></div>\n' +
+    '            <div class="col-xs-6 text-left">Drive to Johannesburg and fly home.</div>\n' +
+    '          </div>\n' +
     '        </div>\n' +
     '      </div>\n' +
     '\n' +
@@ -171,7 +258,7 @@ try {
   module = angular.module('nc-wedding.templates', []);
 }
 module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('travel/travel.template.html',
+  $templateCache.put('registry/registry.template.html',
     '');
 }]);
 })();
@@ -183,7 +270,7 @@ try {
   module = angular.module('nc-wedding.templates', []);
 }
 module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('registry/registry.template.html',
+  $templateCache.put('travel/travel.template.html',
     '');
 }]);
 })();
