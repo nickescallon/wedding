7c44dc154ee0a158f3fde40247101a3591b16bf8
Nick Escallon <nickescallon@gmail.com>
2016-04-22 12:03:33 -0400

    reorganizes images

--- dist/js/templates.js
+++ dist/js/templates.js
@@ -37,6 +37,25 @@ try {
   module = angular.module('nc-wedding.templates', []);
 }
 module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('home/home.template.html',
+    '<div id="home" class="page">\n' +
+    '  <div class="caption">\n' +
+    '    <!-- <div class="col-sm-5 col-xs-3 line"><hr></div> -->\n' +
+    '    <div class="col-xs-6 logo pull-right">Chiara & Nick</div>\n' +
+    '    <!-- <div class="col-sm-5 col-xs-3 line"><hr></div> -->\n' +
+    '  </div>\n' +
+    '</div>\n' +
+    '');
+}]);
+})();
+
+(function(module) {
+try {
+  module = angular.module('nc-wedding.templates');
+} catch (e) {
+  module = angular.module('nc-wedding.templates', []);
+}
+module.run(['$templateCache', function($templateCache) {
   $templateCache.put('itinerary/itinerary.template.html',
     '<div id="itinerary" class="page">\n' +
     '  <div class="wide"></div>\n' +
@@ -229,40 +248,6 @@ try {
   module = angular.module('nc-wedding.templates', []);
 }
 module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('home/home.template.html',
-    '<div id="home" class="page">\n' +
-    '  <div class="caption">\n' +
-    '    <!-- <div class="col-sm-5 col-xs-3 line"><hr></div> -->\n' +
-    '    <div class="col-xs-6 logo pull-right">Chiara & Nick</div>\n' +
-    '    <!-- <div class="col-sm-5 col-xs-3 line"><hr></div> -->\n' +
-    '  </div>\n' +
-    '</div>\n' +
-    '');
-}]);
-})();
-
-(function(module) {
-try {
-  module = angular.module('nc-wedding.templates');
-} catch (e) {
-  module = angular.module('nc-wedding.templates', []);
-}
-module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('photos/photos.template.html',
-    '<div class="text-center">\n' +
-    '  <h1>COMING SOON</h1>\n' +
-    '</div>\n' +
-    '');
-}]);
-})();
-
-(function(module) {
-try {
-  module = angular.module('nc-wedding.templates');
-} catch (e) {
-  module = angular.module('nc-wedding.templates', []);
-}
-module.run(['$templateCache', function($templateCache) {
   $templateCache.put('nav/nav.template.html',
     '<div class="navbar navbar-inverse navbar-fixed-top">\n' +
     '  <div class="container">\n' +
@@ -295,28 +280,43 @@ try {
   module = angular.module('nc-wedding.templates', []);
 }
 module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('photos/photos.template.html',
+    '<div class="text-center">\n' +
+    '  <h1>COMING SOON</h1>\n' +
+    '</div>\n' +
+    '');
+}]);
+})();
+
+(function(module) {
+try {
+  module = angular.module('nc-wedding.templates');
+} catch (e) {
+  module = angular.module('nc-wedding.templates', []);
+}
+module.run(['$templateCache', function($templateCache) {
   $templateCache.put('registry/registry.template.html',
     '<div id="registry" class="page">\n' +
     '  <ul>\n' +
     '    <li>\n' +
     '      <a target="_blank"  href="//www.crateandbarrel.com/gift-registry/nicolas-escallon-and-chiara-castellucci/r5496614">\n' +
-    '        <img src="/images/cb-logo.png"></img>\n' +
+    '        <img src="/images/registry/cb-logo.png"></img>\n' +
     '      </a>\n' +
     '    </li>\n' +
     '\n' +
     '    <li>\n' +
     '      <a target="_blank"  href="//www1.bloomingdales.com/registry/wedding/guest/?registryId=6560559">\n' +
-    '        <img src="/images/bl-logo.png"></img>\n' +
+    '        <img src="/images/registry/bl-logo.png"></img>\n' +
     '      </a>\n' +
     '    </li>\n' +
     '    <li>\n' +
     '      <a target="_blank"  href="https://secure.williams-sonoma.com/registry/2lcbclwsvd/registry-list.html">\n' +
-    '        <img src="/images/ws-logo.png"></img>\n' +
+    '        <img src="/images/registry/ws-logo.png"></img>\n' +
     '      </a>\n' +
     '    </li>\n' +
     '    <li>\n' +
     '      <a target="_blank"  href="https://www.amazon.com/gp/registry/wedding/I2IM8OFIT19K">\n' +
-    '        <img src="/images/am-logo.png"></img>\n' +
+    '        <img src="/images/registry/am-logo.png"></img>\n' +
     '      </a>\n' +
     '    </li>\n' +
     '  </ul>\n' +
@@ -652,7 +652,7 @@ module.run(['$templateCache', function($templateCache) {
     '        <div class="flex-item">\n' +
     '          <a href="//www.presidenthotel.co.za/" target="_blank">\n' +
     '            <div class="row primary">\n' +
-    '              <img class="img-responsive" src="/images/hotel_president.jpg"/>\n' +
+    '              <img class="img-responsive" src="/images/hotel/president.jpg"/>\n' +
     '              <div class="col-md-8 col-md-offset-2">\n' +
     '                <h3 class="text-center">President Hotel</h3>\n' +
     '                <div class="separator"></div>\n' +
@@ -685,7 +685,7 @@ module.run(['$templateCache', function($templateCache) {
     '        <div class="flex-item">\n' +
     '          <a href="//www.radissonblu.com/en/hotel-capetown" target="_blank">\n' +
     '            <div class="row primary">\n' +
-    '              <img class="img-responsive" src="/images/hotel_radisson.jpg"/>\n' +
+    '              <img class="img-responsive" src="/images/hotel/radisson.jpg"/>\n' +
     '              <div class="col-md-8 col-md-offset-2">\n' +
     '                <h3 class="text-center">Radisson Blu Hotel</h3>\n' +
     '                <div class="separator"></div>\n' +
@@ -717,7 +717,7 @@ module.run(['$templateCache', function($templateCache) {
     '        <div class="flex-item">\n' +
     '          <a href="//www.12apostleshotel.com/" target="_blank">\n' +
     '            <div class="row primary">\n' +
-    '              <img class="img-responsive" src="/images/hotel_12_apostles.jpg"/>\n' +
+    '              <img class="img-responsive" src="/images/hotel/12_apostles.jpg"/>\n' +
     '              <div class="col-md-8 col-md-offset-2">\n' +
     '                <h3 class="text-center">12 Apostles</h3>\n' +
     '                <div class="separator"></div>\n' +
@@ -749,7 +749,7 @@ module.run(['$templateCache', function($templateCache) {
     '        <div class="flex-item">\n' +
     '          <a href="//peninsula.co.za/" target="_blank">\n' +
     '            <div class="row primary">\n' +
-    '              <img class="img-responsive" src="/images/hotel_peninsula.jpg"/>\n' +
+    '              <img class="img-responsive" src="/images/hotel/peninsula.jpg"/>\n' +
     '              <div class="col-md-8 col-md-offset-2">\n' +
     '                <h3 class="text-center">Peninsula Hotel</h3>\n' +
     '                <div class="separator"></div>\n' +
@@ -781,7 +781,7 @@ module.run(['$templateCache', function($templateCache) {
     '        <div class="flex-item">\n' +
     '          <a href="//www.rhondasmanor.co.za/" target="_blank">\n' +
     '            <div class="row primary">\n' +
-    '              <img class="img-responsive" src="/images/hotel_rhonda.jpg"/>\n' +
+    '              <img class="img-responsive" src="/images/hotel/rhonda.jpg"/>\n' +
     '              <div class="col-md-8 col-md-offset-2">\n' +
     '                <h3 class="text-center">Rhonda\'s Manor</h3>\n' +
     '                <div class="separator"></div>\n' +
@@ -825,7 +825,7 @@ module.run(['$templateCache', function($templateCache) {
     '        <div class="flex-item">\n' +
     '          <a href="//www.airbnb.com/s/Cape-Town--South-Africa?s_tag=XJQxZgnP" target="_blank">\n' +
     '            <div class="row primary">\n' +
-    '              <img class="img-responsive" src="/images/hotel_airbnb.jpg"/>\n' +
+    '              <img class="img-responsive" src="/images/hotel/airbnb.jpg"/>\n' +
     '              <div class="col-md-8 col-md-offset-2">\n' +
     '                <h3 class="text-center">AirBnB</h3>\n' +
     '                <div class="separator"></div>\n' +
@@ -867,7 +867,7 @@ module.run(['$templateCache', function($templateCache) {
     '        <div class="flex-item">\n' +
     '          <a href="//www.property24.com/to-rent" target="_blank">\n' +
     '            <div class="row primary">\n' +
-    '              <img class="img-responsive" src="/images/hotel_house.jpg"/>\n' +
+    '              <img class="img-responsive" src="/images/hotel/house.jpg"/>\n' +
     '              <div class="col-md-8 col-md-offset-2">\n' +
     '                <h3 class="text-center">House Rentals</h3>\n' +
     '                <div class="separator"></div>\n' +
